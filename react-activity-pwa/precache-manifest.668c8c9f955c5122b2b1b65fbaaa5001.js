b9771eea4db0648003b77895de4ddbac2f0869ff
jim <thecastlefaller@gmail.com>
2019-02-01 03:40:57 +0800

    update pwa

---
+++ react-activity-pwa/precache-manifest.668c8c9f955c5122b2b1b65fbaaa5001.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc56412e586d32800f3f",
+    "url": "/react-activity-pwa/static/js/runtime~main.dc56412e.js"
+  },
+  {
+    "revision": "1d2bcb3934f0590a9e39",
+    "url": "/react-activity-pwa/static/js/main.1d2bcb39.chunk.js"
+  },
+  {
+    "revision": "148d151286fa0fda5e4e",
+    "url": "/react-activity-pwa/static/js/1.148d1512.chunk.js"
+  },
+  {
+    "revision": "462f6dcdc005433ebf7e7687263694e8",
+    "url": "/react-activity-pwa/index.html"
+  }
+];
